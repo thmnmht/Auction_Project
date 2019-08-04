17ff2076737597f6cbc52334bdf14ee872f94e0d
Sobhan Ebrahimi <sobhanebrahimi36@yahoo.com>
2019-08-04 11:11:46 +0430

    flyway-initialize

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -7,8 +7,51 @@ CREATE TABLE Categories
 
 CREATE TABLE Pictures
 (
-    id      INT          NOT NULL AUTO_INCREMENT,
+    id       INT          NOT NULL AUTO_INCREMENT,
     filename VARCHAR(100) NOT NULL,
-    date    DATE,
+    date     DATE         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Bids
+(
+    id          INT  NOT NULL AUTO_INCREMENT,
+    auction_uid INT  NOT NULL,
+    user_uid    INT  NOT NULL,
+    price       INT  NOT NULL,
+    date        DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE Auctions
+(
+    id           INT         NOT NULL AUTO_INCREMENT,
+    title        VARCHAR(50) NOT NULL,
+    description  VARCHAR(1000),
+    base_price   INT         NOT NULL,
+    category_uid INT         NOT NULL,
+    date         DATE        NOT NULL,
+    state        VARCHAR(20) NOT NULL,
+    winner       INT,
+    picture_uid  INT         NOT NULL,
+    owner        INT         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Login_infos
+(
+    id       INT  NOT NULL AUTO_INCREMENT,
+    user_uid INT  NOT NULL,
+    date     DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Users
+(
+    id        INT          NOT NULL AUTO_INCREMENT,
+    name      VARCHAR(40)  NOT NULL,
+    email     VARCHAR(40)  NOT NULL,
+    password  VARCHAR(100) NOT NULL,
+    picture   VARCHAR(100),
+    bookmarks INT,
     PRIMARY KEY (id)
 );
