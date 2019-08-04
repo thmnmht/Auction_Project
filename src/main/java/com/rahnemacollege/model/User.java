1b76685af7f9dc3c5953f70c613097c11d57e3a9
tahmine <tmohati@gmail.com>
2019-08-04 11:57:12 +0430

    changes

--- src/main/java/com/rahnemacollege/model/User.java
+++ src/main/java/com/rahnemacollege/model/User.java
@@ -4,13 +4,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class Users {
+public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String name;
     private String email;
     private String password;
-    
+    private String picture;
+
+
 
 }
