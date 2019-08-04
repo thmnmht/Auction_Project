9fcbe8fa3fe03dbaac0184988c377beecc19252c
Sobhan Ebrahimi <sobhanebrahimi36@yahoo.com>
2019-08-04 13:18:22 +0430

    get all of the users added!

--- src/main/java/com/rahnemacollege/model/User.java
+++ src/main/java/com/rahnemacollege/model/User.java
@@ -4,13 +4,16 @@ import lombok.Data;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import java.util.Set;
 
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    @Email
     private String email;
     private String password;
     private String picture;
