ac14922bea825e0d3b7cd24b5beadbb48e729884
Sobhan Ebrahimi <sobhanebrahimi36@yahoo.com>
2019-08-07 12:00:52 +0430

    encryption added!

--- src/main/java/com/rahnemacollege/service/UserService.java
+++ src/main/java/com/rahnemacollege/service/UserService.java
@@ -3,6 +3,8 @@ package com.rahnemacollege.service;
 import com.rahnemacollege.domain.UserDomain;
 import com.rahnemacollege.model.User;
 import com.rahnemacollege.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,24 +14,29 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository repository;
-    public UserService(UserRepository repository) {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User addUser(UserDomain userDomain){
         User user = toUser(userDomain);
+        user.setPassword(passwordEncoder.encode(userDomain.getPassword()));
         repository.save(user);
         return user;
     }
 
     public List<User> getAll() {
         ArrayList<User> users = new ArrayList<>();
-        repository.findAll().forEach(u -> users.add(u));
+        repository.findAll().forEach(users::add);
         return users;
     }
 
     public User toUser(UserDomain userDomain){
-        return new User(userDomain.getName(),userDomain.getEmail(),userDomain.getPassword());
+        return new User(userDomain.getName(),userDomain.getEmail(),passwordEncoder.encode(userDomain.getPassword()));
     }
     public Optional<com.rahnemacollege.model.User> findById(int id) {
         return repository.findById(id);
