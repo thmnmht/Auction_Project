bcf7219f8561a246e659a58277b45a02a5172acf
Sobhan Ebrahimi <sobhanebrahimi36@yahoo.com>
2019-08-27 14:57:49 +0430

    added Second user

--- src/main/java/com/rahnemacollege/repository/UserRepository.java
+++ src/main/java/com/rahnemacollege/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.rahnemacollege.repository;
 
 import com.rahnemacollege.model.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
