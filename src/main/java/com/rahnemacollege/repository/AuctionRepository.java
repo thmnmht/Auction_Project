5e6e26a0a9326b0a03abce688e74e75bc64164c8
tahmine <tmohati@gmail.com>
2019-09-02 15:10:11 +0430

    remove filters and use repository methods instead

--- src/main/java/com/rahnemacollege/repository/AuctionRepository.java
+++ src/main/java/com/rahnemacollege/repository/AuctionRepository.java
@@ -14,18 +14,28 @@ public interface AuctionRepository extends CrudRepository<Auction, Integer> {
     @Query(value = " SELECT * ,COUNT(bookmarks_id) AS number_of_bookmarks " +
             "FROM Auctions left join users_bookmarks " +
             "on (Auctions.id = users_bookmarks.bookmarks_id ) " +
-            "WHERE Auctions.state!=1 " +
+            "WHERE Auctions.state!=1 AND Auctions.title like %?1% " +
             "group by Auctions.id " +
             "ORDER BY number_of_bookmarks DESC, Auctions.id DESC\n",
             countQuery = "SELECT count(*) FROM Auctions",
             nativeQuery = true)
-    Page<Auction> findHottest(Pageable pageable);
+    Page<Auction> findHottest(String title, Pageable pageable);
+
+    @Query(value = " SELECT * ,COUNT(bookmarks_id) AS number_of_bookmarks " +
+            "FROM Auctions left join users_bookmarks " +
+            "on (Auctions.id = users_bookmarks.bookmarks_id ) " +
+            "WHERE Auctions.state != 1 And Auctions.category_id = ?1 AND Auctions.title like %?2% " +
+            "group by Auctions.id " +
+            "ORDER BY number_of_bookmarks DESC, Auctions.id DESC\n",
+            countQuery = "SELECT count(*) FROM Auctions",
+            nativeQuery = true)
+    Page<Auction> findHottestByCategoryId(int categoryId, String title, Pageable pageable);
 
     Page<Auction> findByOwner_idOrderByIdDesc(int userId, Pageable pageable);
 
-    Page<Auction> findByStateNotOrderByIdDesc(int state, Pageable pageable);
+    Page<Auction> findByStateNotAndTitleContainingOrderByIdDesc(int state, String title, Pageable pageable);
 
-    Page<Auction> findByStateNotAndCategory_idOrderByIdDesc(int state, int CategoryId, Pageable pageable);
+    Page<Auction> findByStateNotAndCategory_idAndTitleContainingOrderByIdDesc(int state, int CategoryId, String title, Pageable pageable);
 
 
 }
